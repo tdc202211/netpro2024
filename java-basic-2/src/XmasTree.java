db1f9d6a0c0da7379a6cf2486de1e1b2838f93c7
tdc202211 <tdc202211@gmail.com>
2024-04-26 12:17:46 +0900

    a

--- java-basic-2/src/XmasTree.java
+++ java-basic-2/src/XmasTree.java
@@ -1,11 +1,45 @@
+import java.util.Scanner;
+
 public class XmasTree {
 
     public static void main(String[] args) {
-        int X = Integer.parseInt(args[0]);
-        int Y = Integer.parseInt(args[1]);
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        String[] word = str.split(",");
 
-        System.out.println("Draw your own xmas tree with multi-loop!");
+        for (int k = 1; k <= Integer.parseInt(word[0]); k++) {
 
+            for (int i = 1; i <= Integer.parseInt(word[0]) - k; i++) // 左側の余白作り
+                if (i % 2 != 0) {
+                    System.out.print(word[3]);
+                } else {
+                    System.out.print(" ");
+                }
+
+            for (int i = 1; i <= 2 * k - 1; i++) // ツリーの横一列
+                System.out.print("*");
+
+            for (int i = 1; i <= Integer.parseInt(word[0]) - k; i++) // 左側の余白作り
+                if (i % 2 != 0) {
+                    System.out.print(word[3]);
+                } else {
+                    System.out.print(" ");
+                }
+
+            System.out.println(" "); // 改行
+        }
+
+        for (int k = 1; k <= Integer.parseInt(word[1]); k++) {
+
+            for (int i = 1; i <= Integer.parseInt(word[0]) - Math.ceil(Double.parseDouble(word[2]) / 2.0); i++) // 左側の余白作り
+                System.out.print(" ");
+
+            for (int i = 1; i <= Integer.parseInt(word[2]); i++)
+                System.out.print("*");
+
+            System.out.println();
+        }
     }
+
 }
 // https://docs.oracle.com/javase/jp/8/docs/api/java/lang/Integer.html
